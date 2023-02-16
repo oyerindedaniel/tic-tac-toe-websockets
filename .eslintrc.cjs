0434042062ddba0c908b6664236d98cd3e6ed3bb
oyerindedaniel <oyerinde.daniel@yahoo.com>
2023-02-16 15:04:32 +0100

    inital commit �

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-nested-ternary': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-return-assign': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'object-curly-newline': 'off',
+    'comma-dangle': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'operator-linebreak': 'off',
+    'no-confusing-arrow': 'off',
+    'function-paren-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'react/no-unused-prop-types': 'off',
+    'max-len': 'off',
+    'no-shadow': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/no-unknown-property': 'off',
+    'linebreak-style': 'off',
+    indent: 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# TIC-TAC-TOE using WEBSOCKETS
+
+This is a real-time multiplayer Tic-Tac-Toe game that uses WebSockets to enable communication between clients and server. The game allows two players to play against each other in real-time, with their moves being synchronized across all connected clients.
+
+## Usage
+
+To play the game:
+
+1. Enter your name and click "Start Game"
+2. Wait for another player to join the game
+3. Take turns marking X's and O's on the game board
+4. The first player to get three in a row wins the game
